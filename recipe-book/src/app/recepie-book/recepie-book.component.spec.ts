f2af73db4fb71f03b94f49587f7f687bccf5bacf
KostaLaz <kosta.lazarevski89@gmail.com>
2021-03-29 00:37:09 +0200

    recipe-book component added

---
+++ recipe-book/src/app/recepie-book/recepie-book.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { RecepieBookComponent } from './recepie-book.component';
+
+describe('RecepieBookComponent', () => {
+  let component: RecepieBookComponent;
+  let fixture: ComponentFixture<RecepieBookComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ RecepieBookComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(RecepieBookComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
