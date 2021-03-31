1eb87ed4b92600bbf6aa4ba2c576a44227768700
KostaLaz <kosta.lazarevski89@gmail.com>
2021-04-01 00:48:03 +0200

    load items in recipe-details section

--- recipe-book/src/app/recepie-book/recepie-book.component.ts
+++ recipe-book/src/app/recepie-book/recepie-book.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from './recipe.model';
 
 @Component({
   selector: 'app-recepie-book',
@@ -6,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recepie-book.component.css']
 })
 export class RecepieBookComponent implements OnInit {
+  selectedRecipe: Recipe;
 
   constructor() { }
 
