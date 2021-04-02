43104ced4adda7f1d3d93e96024f51c0750cc9e2
KostaLaz <kosta.lazarevski89@gmail.com>
2021-04-02 14:20:41 +0200

    display recipes in recipe list using recipeService

--- recipe-book/src/app/recepie-book/recepie-book.component.ts
+++ recipe-book/src/app/recepie-book/recepie-book.component.ts
@@ -1,10 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { Recipe } from './recipe.model';
+import { RecipeBookService } from './recipe.service';
 
 @Component({
   selector: 'app-recepie-book',
   templateUrl: './recepie-book.component.html',
-  styleUrls: ['./recepie-book.component.css']
+  styleUrls: ['./recepie-book.component.css'],
+  providers: [RecipeBookService]
 })
 export class RecepieBookComponent implements OnInit {
   selectedRecipe: Recipe;
--- recipe-book/src/app/recepie-book/recipe-list/recipe-list.component.ts
+++ recipe-book/src/app/recepie-book/recipe-list/recipe-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { Recipe } from '../recipe.model';
+import { RecipeBookService } from '../recipe.service';
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
@@ -7,14 +8,12 @@ import { Recipe } from '../recipe.model';
 })
 export class RecipeListComponent implements OnInit {
   @Output() recipeWasSelected = new EventEmitter<Recipe>();
-  recipes: Recipe[] = [
-    new Recipe('a Test recipe', 'TestDecription', 'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?webp=true&quality=90&resize=620%2C563'),
-    new Recipe('TestRecipe', 'TestDescription', 'https://static01.nyt.com/images/2020/01/29/dining/27Romanrex/merlin_167079174_ecc032e7-071b-44ab-8852-0d5025d0e90d-articleLarge.jpg')
-  ]
+  recipes: Recipe[];
 
-  constructor() { }
+  constructor(private recipeBookService: RecipeBookService) { }
 
   ngOnInit(): void {
+    this.recipes = this.recipeBookService.getRecipes();
   }
 
   onRecipeSelcted(recipe: Recipe){
