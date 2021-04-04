04fad6f1615063ef0e17f0c2b8d4a7bc89cce486
KostaLaz <kosta.lazarevski89@gmail.com>
2021-04-04 23:12:41 +0200

    add ingredients to shopping-list logic

--- recipe-book/src/app/recepie-book/recipe.service.ts
+++ recipe-book/src/app/recepie-book/recipe.service.ts
@@ -1,21 +1,23 @@
-import { EventEmitter } from "@angular/core";
+import { EventEmitter, Injectable } from "@angular/core";
 import { Ingredient } from "../shared/ingredient.model";
+import { ShoppingListService } from "../shopping-list/shopping-list.service";
 import { Recipe } from "./recipe.model";
 
+@Injectable()
 export class RecipeBookService{
     recipeSelectd = new EventEmitter<Recipe>()
 
     recipes: Recipe[] = [
-        new Recipe('a Test recipe',
-         'TestDecription',
+        new Recipe('Shakshuka',
+         'Traditional Indian dish',
          'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?webp=true&quality=90&resize=620%2C563',
          [
            new Ingredient('Eggs', 8),
            new Ingredient('Basil', 5),
            new Ingredient('Tomato', 12)
          ]),
-        new Recipe('TestRecipe',
-         'TestDescription',
+        new Recipe('Pesto Spagetti',
+         'Beautiful, fresh, Italian pasta',
           'https://static01.nyt.com/images/2020/01/29/dining/27Romanrex/merlin_167079174_ecc032e7-071b-44ab-8852-0d5025d0e90d-articleLarge.jpg',
           [
             new Ingredient('Spagetti', 0.5),
@@ -24,7 +26,14 @@ export class RecipeBookService{
           ])
       ]
 
+      constructor(private shoppingListService: ShoppingListService){}
+
       getRecipes(){
         return this.recipes.slice();
 }
+
+addIngredientsToShoppingList(ingredients: Ingredient[]){
+    this.shoppingListService.addIngredients(ingredients);
+}
+
 }
