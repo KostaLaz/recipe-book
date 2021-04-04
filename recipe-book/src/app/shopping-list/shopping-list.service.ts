b1fa61928c18f7ae93c31ba3d1ee25f98eebd051
KostaLaz <kosta.lazarevski89@gmail.com>
2021-04-04 20:36:22 +0200

    get and add ingrendiesnt logic

--- recipe-book/src/app/shopping-list/shopping-list.service.ts
+++ recipe-book/src/app/shopping-list/shopping-list.service.ts
@@ -1,3 +1,19 @@
+import { EventEmitter } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
+
 export class ShoppingListService{
-    
+    ingrediantsChanged = new EventEmitter<Ingredient[]>();
+   private ingredients: Ingredient[] = [
+        new Ingredient('salt', 10),
+        new Ingredient('peper', 20)
+      ]; 
+
+      getIngredients(){
+          return this.ingredients.slice();
+      }
+
+      addIngredient(ingredient: Ingredient){
+          this.ingredients.push(ingredient);
+          this.ingrediantsChanged.emit(this.ingredients.slice());
+      }
 }
