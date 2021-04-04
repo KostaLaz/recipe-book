e38c3e6761920f16150e05319170de9f06f59e8c
KostaLaz <kosta.lazarevski89@gmail.com>
2021-04-04 20:58:57 +0200

    ingredients added

--- recipe-book/src/app/recepie-book/recipe.service.ts
+++ recipe-book/src/app/recepie-book/recipe.service.ts
@@ -1,4 +1,5 @@
 import { EventEmitter } from "@angular/core";
+import { Ingredient } from "../shared/ingredient.model";
 import { Recipe } from "./recipe.model";
 
 export class RecipeBookService{
@@ -8,11 +9,19 @@ export class RecipeBookService{
         new Recipe('a Test recipe',
          'TestDecription',
          'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?webp=true&quality=90&resize=620%2C563',
-         []),
+         [
+           new Ingredient('Eggs', 8),
+           new Ingredient('Basil', 5),
+           new Ingredient('Tomato', 12)
+         ]),
         new Recipe('TestRecipe',
          'TestDescription',
           'https://static01.nyt.com/images/2020/01/29/dining/27Romanrex/merlin_167079174_ecc032e7-071b-44ab-8852-0d5025d0e90d-articleLarge.jpg',
-          [])
+          [
+            new Ingredient('Spagetti', 0.5),
+            new Ingredient('Parmezan', 1),
+            new Ingredient('Basil', 5)
+          ])
       ]
 
       getRecipes(){
