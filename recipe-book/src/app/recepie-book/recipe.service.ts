8749024af621ac8d747237ac5b26287948d2403a
KostaLaz <kosta.lazarevski89@gmail.com>
2021-04-04 20:45:26 +0200

    adding ingrediats to recipe

--- recipe-book/src/app/recepie-book/recipe.service.ts
+++ recipe-book/src/app/recepie-book/recipe.service.ts
@@ -5,8 +5,14 @@ export class RecipeBookService{
     recipeSelectd = new EventEmitter<Recipe>()
 
     recipes: Recipe[] = [
-        new Recipe('a Test recipe', 'TestDecription', 'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?webp=true&quality=90&resize=620%2C563'),
-        new Recipe('TestRecipe', 'TestDescription', 'https://static01.nyt.com/images/2020/01/29/dining/27Romanrex/merlin_167079174_ecc032e7-071b-44ab-8852-0d5025d0e90d-articleLarge.jpg')
+        new Recipe('a Test recipe',
+         'TestDecription',
+         'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?webp=true&quality=90&resize=620%2C563',
+         []),
+        new Recipe('TestRecipe',
+         'TestDescription',
+          'https://static01.nyt.com/images/2020/01/29/dining/27Romanrex/merlin_167079174_ecc032e7-071b-44ab-8852-0d5025d0e90d-articleLarge.jpg',
+          [])
       ]
 
       getRecipes(){
